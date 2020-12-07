94888d72a74dfc64bdf5a96b736850fdd210523e
Syukrirahiman <43146706+Syukrirahiman@users.noreply.github.com>
2020-12-07 15:18:58 +0800

    Created example components

--- hello-world/src/app/app-routing.module.ts
+++ hello-world/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CourseComponent } from './course/course.component';
+import { CoursesComponent } from './courses.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'course', component: CourseComponent},
+  {path: 'courses', component: CoursesComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const routingComponents = [CourseComponent, CoursesComponent]
--- hello-world/src/app/app.module.ts
+++ hello-world/src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoursesService } from './courses.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import  { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    routingComponents
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
-  providers: [],
+  providers: [
+    CoursesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
